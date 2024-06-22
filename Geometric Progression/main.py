b409f32188c3cd4b46d3adee40a568eaa00d8aff
Khushal Sharma <121273008+KhushalSharmaTheDevloper@users.noreply.github.com>
2024-06-22 09:30:08 +0530

    Add files via upload

---
+++ Geometric Progression/main.py
@@ -0,0 +1,11 @@
+import math
+# ^^ => this was premade python plugin
+def gb(x,y,n):          ##Function
+    return(x*(int)(math.pow(y, N-1)))  ## It multiplies x by y raised to the power of n-1.
+
+x = 2  ##This is 1st term
+y = 5   ## Whome to multipliy with
+N = 3   ##Nth Term
+
+print ("The" , N , "th term in the series is :" , gb(x,y,N))
+                                                ##^^^^^^^^^^ :=> This is repearting Formula from line 3.                 
